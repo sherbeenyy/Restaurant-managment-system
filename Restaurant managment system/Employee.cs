55a93b4499707f9b64c1834c071f699f366292f4
Ahmed Hesham <ahmedhesham11m@gmail.com>
2024-03-05 02:47:23 +0200

    Add project files.

---
+++ Restaurant managment system/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_managment_system
+{
+    internal class Employee
+    {
+        private int _id;
+        private string _role;
+        private string _name;
+        private int _age; // i thought when the owner will hire he should see if he's above 18 or not;
+        private string _address;
+        private string _phoneNumber;
+        private float _workingHours;
+        private float _salary;
+        private int _shift; // 0 representing night and 1 representing morning ;
+        static private int _totalEmployees = 0;
+
+       public Employee(int id, string role, string name, int age, string address, string phoneNumber, float workingHours, float salary, int shift)
+        {
+            _id = id;
+            _role = role;
+            _name = name;
+            _age = age;
+            _address = address;
+            _phoneNumber = phoneNumber;
+            _workingHours = workingHours;
+            _salary = workingHours*12.2f;
+            _shift = shift;
+            _totalEmployees++;
+        }
+
+        public int Age
+        {
+            set { if (Age >= 18)
+                {
+                    Age = value;
+
+                }else
+                {
+                    Age = -1; //invalid age so we can detect and fire him ;
+                }
+            }
+            get { return _age; }
+        }  
+    }    
+}
---
+++ Restaurant managment system/Owner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_managment_system
+{
+
+
+    internal class Owner
+    {
+        List<Employee> employees; //data strcute like the array but easier to add and delete ;
+
+        public bool checkIfValid(Employee e)
+        {
+
+            // INCOMPLETE !!  we should check for other parameters 
+            if (e.Age == -1)
+            { return false; }
+
+            return true;
+        }
+
+        public void hire(Employee e)
+        {
+            if (checkIfValid(e))
+            {
+                employees.Add(e);
+            }
+        }
+
+    }
+}
