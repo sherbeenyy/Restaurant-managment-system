e2479324ffa93d878eb2427e4ffe85dd197a1f80
Uncolaed <mazenbelal2023@gmail.com>
2024-03-07 15:24:19 +0200

    removed semi colons from comments, also hi ahmed

--- Restaurant managment system/Employee.cs
+++ Restaurant managment system/Employee.cs
@@ -12,12 +12,12 @@ namespace Restaurant_managment_system
         private int _id;
         private string _role;
         private string _name;
-        private int _age; // i thought when the owner will hire he should see if he's above 18 or not;
+        private int _age; // i thought when the owner will hire he should see if he's above 18 or not
         private string _address;
         private string _phoneNumber;
         private float _workingHours;
         private float _salary = 0;
-        private int _shift; // 0 representing night and 1 representing morning ;
+        private int _shift; // 0 representing night and 1 representing morning
         static private int _totalEmployees = 0;
 
        public Employee(int id, string role, string name, int age, string address, string phoneNumber, float workingHours, int shift)
