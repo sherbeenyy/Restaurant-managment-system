a54766a4774db302804fec0c2704ab701a9f1613
Uncolaed <mazenbelal2023@gmail.com>
2024-03-21 21:42:55 +0200

    added menu class and working on testing

---
+++ Restaurant managment system/Menu.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class Menu
+    {
+    // variables
+
+    private string _food_name;
+    private string _food_description;
+    private decimal _food_price;
+    private string _food_category;
+
+
+    // constructor 
+    public Menu(string food_name, string food_description, decimal food_price, string food_category)
+    {
+        _food_name = food_name;
+        _food_description = food_description;
+        _food_price = food_price;
+        _food_category = food_category;
+    }
+    public Menu()
+    {
+
+
+    }
+
+
+
+    // get and sets for the new menu 
+
+    public string FoodName 
+
+    { get 
+        { return _food_name; } 
+        set 
+        { _food_name = value; } 
+    }
+
+    public string FoodDescription
+    {
+        get
+        { return _food_description; }
+        set
+        {
+            _food_description = value;     
+        }
+    }
+
+    public decimal FoodPrice
+    {
+        get 
+        { 
+            return _food_price; 
+        }
+        set
+        
+        {
+            _food_price = value; 
+        }
+    }
+
+    public string FoodCategory
+    {
+        get 
+        { 
+            return _food_category; 
+        }
+        set
+        { _food_category = value; }
+    }
+
+}
+
--- Restaurant managment system/Program.cs
+++ Restaurant managment system/Program.cs
@@ -6,9 +6,14 @@ class Program
     static void Main(string[] args)
     {
 
+        // Employee calss testing 
         Employee ahmed = new Employee(1, "chef", "ahmed", 20, "cairo", "01111111", 12, 0);
         Employee ashraf = new Employee(1, "chef", "ashraf", 20, "cairo", "01111111", 12, 0);
         Employee osama = new Employee(1, "cashier", "osama", 20, "cairo", "01111111", 12, 0);
+
+
+
+        ahmed.displayInfo();
 
         Owner Mohamed = new Owner(1, "owner", "Mohamed", 20, "cairo", "01111111", 12, 0, 0);
         Mohamed.hire(ahmed);
@@ -20,5 +25,17 @@ class Program
         Console.WriteLine("Employees: ");
         Mohamed.printEmployees();
         ashrafm.hire(ahmed);
+
+
+        // Menu class testing 
+
+        Console.WriteLine("\n------------Menu testing ---------------\n");
+
+        Menu item1 = new Menu();
+        Menu item2 = new Menu();
+        Menu item3 = new Menu();
+        Console.WriteLine("1. Add new item.\n2. Remove item. \n3. Edit item.\n4. View item details.\n");
+        Console.Write(">>");
+        int choice = int.Parse(Console.ReadLine());
     }
 }
